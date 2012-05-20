ff5e6fdfa87bbfaf8376908c217b984d417a3e7d
kbinani <kbinani@msn.com>
2012-05-21 01:32:40 +0900

    modify scala output layout

--- php2scala.php
+++ php2scala.php
@@ -1,8 +1,9 @@
 <?php
 
 require_once __DIR__ . '/lib/Converter.php';
+
 $file = $argv[1];
 $code = trim(file_get_contents($file));
 
 $converter = new Converter();
-echo $converter->convert($argv[0], $code, $file);
+echo $converter->convert( $code );
